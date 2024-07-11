c4a04354c681d93ca99eeb10c2869f303aef8c25
sampatk20 <sampatk2002@gmail.com>
2024-07-10 17:50:59 -0700

    Initial commit

---
+++ src/Components/Contact.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const email = 'udugula@usc.edu'
+const Contact = () => {
+    return (
+      <div name='contact' className='w-full h-screen bg-[#ffffff] flex justify-center items-center p-4'>
+          <form method='POST' action="https://getform.io/f/56d0eb9d-cf2c-46ec-8dc4-f114ef0aa40a" className='flex flex-col max-w-[600px] w-full'>
+              <div className='pb-8'>
+                  <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-black-300'>Contact</p>
+                  <p className='text-black-300 py-4'>// Submit the form below or shoot me an email to <a href={`mailto:${email}`} style={{ textDecoration: 'underline', color: '#43A5BE' }}>{email}</a></p>
+              </div>
+              <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
+              <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
+              <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea>
+              {/* <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button> */}
+              <button className='text-white bg-blue-500 border-2 border-blue-500 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
+
+          </form>
+      </div>
+    )
+  }
+
+export default Contact
---
+++ src/Pages/About.js
@@ -0,0 +1,53 @@
+import React from "react";
+import Work from "../Components/Work";
+import { personalDetails, workDetails, eduDetails } from "../Details";
+
+function About() {
+  return (
+    <main className="container mx-auto max-width pt-10 pb-20 ">
+      <section>
+        <h1 className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
+          About Me
+        </h1>
+        {/* <p className="text-content py-8 lg:max-w-3xl">{personalDetails.about}</p> */}
+        <p className="text-content py-8 lg:max-w-3xl" dangerouslySetInnerHTML={{ __html: personalDetails.about }}></p>
+      </section>
+      <section>
+        <h1 className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
+          Work Experience
+        </h1>
+        {React.Children.toArray(
+          workDetails.map(({ Position, Company, Location, Type, Details, Duration }) => (
+            <Work
+              position={Position}
+              company={Company}
+              location={Location}
+              type={Type}
+              details = {Details}
+              duration={Duration}
+            />
+          ))
+        )}
+      </section>
+      <section>
+        <h1 className="text-2xl pt-10 text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
+          Education
+        </h1>
+        {React.Children.toArray(
+          eduDetails.map(({ Position, Company, Location, Type,Details, Duration }) => (
+            <Work
+              position={Position}
+              company={Company}
+              location={Location}
+              type={Type}
+              details = {Details}
+              duration={Duration}
+            />
+          ))
+        )}
+      </section>
+    </main>
+  );
+}
+
+export default About;
